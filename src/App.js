b2ae3a4550ae42fa057d07a7403768dbeaed0712
andredgibbs <a.gibbs@dewynters.com>
2023-05-19 14:07:46 +0100

    updated body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import styled from 'styled-components';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Layout from './components/Layout/Layout';
 
+export const AppContainer = styled.div`
+    width: 1440px;
+    margin: 0 auto;
+
+    @media (max-width: 768px) {
+      width: auto;
+    }
+`
+
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Header/>
       <Layout/>
       <Footer/>
-    </div>
+    </AppContainer>
   );
 }
 
